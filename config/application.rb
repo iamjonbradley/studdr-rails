001230d0a9d4486ef87e36aa4570917298da7aab
Eddie Lange <lunarhole@gmail.com>
2011-07-22 19:44:23 -0400

    adding OmniAuth

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Studdr
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.middleware.use OmniAuth::Strategies::Twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'TJAQ1utgb7XUcOrb8VOFyA', 'WgtAEOXCt3Z6hQTysiSLjcshQ3lzoLUhBrvPAHdv3Q'
   provider :open_id, OpenID::Store::Filesystem.new('/tmp')
+  provider :facebook, "164277726917774", "97ac5af1a1893aad75446c6ad19e1711"
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol', :identifier => 'openid.aol.com'
+  provider :twitter, "TJAQ1utgb7XUcOrb8VOFyA", "WgtAEOXCt3Z6hQTysiSLjcshQ3lzoLUhBrvPAHdv3Q"
 end
